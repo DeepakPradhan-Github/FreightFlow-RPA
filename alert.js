7821014cb9ad59b81ba0d10c8c86e979654c2636
Ankitnext <ankitnextech3@gmail.com>
2024-07-12 16:26:57 +0530

    text updated
    text updated

--- alert.js
+++ alert.js
@@ -9,11 +9,11 @@ function viewDetails(jobId) {
             var jobDetails = JSON.parse(response);
             if (jobDetails.length > 0) {
                 // Initialize the table string
-                var tableStr = "<table border='1' style='width:100%; text-align: left;'><thead><tr><th>Job Id</th><th>Date</th><th>Source</th><th>Destination</th><th>Container</th><th>Commodity</th><th>Departure Time</th><th>Arrival Time</th><th>Get in Deadline</th><th>Quotation Price</th><th>Estimation Arrival</th><th>Estimation Destination</th><th>Shipping Company</th></tr></thead><tbody>";
+                var tableStr = "<table border='1' style='width:100%; text-align: center; font-size:84%;'><thead><tr><th>Job Id</th><th>Source</th><th>Destination</th><th>Container</th><th>Commodity</th><th>Departure Time</th><th>Arrival Time</th><th>Get in Deadline</th><th>Quotation Price</th><th>Estimation Arrival</th><th>Estimation Destination</th><th>Shipping Company</th></tr></thead><tbody>";
 
                         // Loop through each job and add it to the table string
                         jobDetails.forEach(function(job) {
-                            tableStr += "<tr><td>" + job.job_id + "</td><td>" + job.date + "</td><td>"  + job.source + "</td><td>" + job.destination + "</td><td>" + job.container + "</td><td>" + job.commodity + "</td><td>"+ job.departure_time + "</td><td>"+ job.arrival_time + "</td><td>"+ job.get_in_deadline + "</td><td>"+ job.quotation_price + "</td><td>"+ job.estimation_arrival + "</td><td>"+ job.estimation_destination + "</td><td>" + job.shipping_company + "</td></tr>";
+                            tableStr += "<tr><td>" + job.job_id + "</td><td>"  + job.source + "</td><td>" + job.destination + "</td><td>" + job.container + "</td><td>" + job.commodity + "</td><td>"+ job.departure_time + "</td><td>"+ job.arrival_time + "</td><td>"+ job.get_in_deadline + "</td><td>"+ job.quotation_price + "</td><td>"+ job.estimation_arrival + "</td><td>"+ job.estimation_destination + "</td><td>" + job.shipping_company + "</td></tr>";
                         });
 
                         tableStr += "</tbody></table>";
